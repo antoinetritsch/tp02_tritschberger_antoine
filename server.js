0e99c01586bffb0441be9c92ed0aac850b710029
tritschbergera <antoine.tritschberger@etu.unistra.fr>
2022-01-30 16:44:11 +0100

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/tp2-angular"));
+app.use(express.static(__dirname + "/dist/TP2ANGULAR"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/tp2-angular/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/TP2ANGULAR/index.html"));
 });
 app.listen(process.env.PORT || 8080);
